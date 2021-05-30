e5afc1617b14de6b11aa58b8b9223774815e0fa9
Elham-gh <76242088+Elham-gh@users.noreply.github.com>
2021-05-30 13:15:08 +0430

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,79 +1,155 @@
 import os
 import os.path as osp
 
+from PIL import Image
 import numpy as np
+import scipy.io as sio
 
 import torch
-import torch.nn as nn
 from torch.nn import functional as F
-from torch.nn import DataParallel
-from torch.utils.data import DataLoader
+from torch.utils import data
 
-from dataset import ValDataset 
-from metric import fast_hist, cal_scores
-from network import EMANet 
 import settings
 
-logger = settings.logger
+
+def fetch(image_path, label_path=None):
+    with open(image_path, 'rb') as fp:
+        image = Image.open(fp).convert('RGB')
+    image = torch.FloatTensor(np.asarray(image)) / 255
+    image = (image - settings.MEAN) / settings.STD
+    image = image.permute(2, 0, 1).unsqueeze(dim=0)
+
+    if label_path is not None:
+        with open(label_path, 'rb') as fp:
+            label = Image.open(fp).convert('P')
+        label = torch.FloatTensor(np.asarray(label))
+        label = label.unsqueeze(dim=0).unsqueeze(dim=1)
+    else:
+        label = None
+
+    return image, label
 
 
-class Session:
-    def __init__(self, dt_split):
-        torch.cuda.set_device(settings.DEVICE)
-
-        self.log_dir = settings.LOG_DIR
-        self.model_dir = settings.MODEL_DIR
-
-        self.net = EMANet(settings.N_CLASSES, settings.N_LAYERS).cuda()
-        self.net = DataParallel(self.net, device_ids=[settings.DEVICE])
-        dataset = ValDataset(split=dt_split)
-        self.dataloader = DataLoader(dataset, batch_size=1, shuffle=False, 
-                                     num_workers=2, drop_last=False)
-        self.hist = 0
-
-    def load_checkpoints(self, name):
-        ckp_path = osp.join(self.model_dir, name)
-        try:
-            obj = torch.load(ckp_path, 
-                             map_location=lambda storage, loc: storage.cuda())
-            logger.info('Load checkpoint %s.' % ckp_path)
-        except FileNotFoundError:
-            logger.info('No checkpoint %s!' % ckp_path)
-            return
-
-        self.net.module.load_state_dict(obj['net'])
-
-    def inf_batch(self, image, label):
-        image = image.cuda()
-        label = label.cuda()
-        with torch.no_grad():
-            logit = self.net(image)
-
-        pred = logit.max(dim=1)[1]
-        self.hist += fast_hist(label, pred)
+def scale(image, label=None):
+    ratio = np.random.choice(settings.SCALES)
+    image = F.interpolate(image, scale_factor=ratio, mode='bilinear', 
+                          align_corners=True)
+    if label is not None:
+        label = F.interpolate(label, scale_factor=ratio, mode='nearest')
+    return image, label
 
 
-def main(ckp_name='latest.pth'):
-    sess = Session(dt_split='val')
-    sess.load_checkpoints(ckp_name)
-    dt_iter = sess.dataloader
-    sess.net.eval()
+def pad(image, label=None):
+    h, w = image.size()[-2:] 
+    crop_size = settings.CROP_SIZE
+    pad_h = max(crop_size - h, 0)
+    pad_w = max(crop_size - w, 0)
+    if pad_h > 0 or pad_w > 0:
+        image = F.pad(image, (0, pad_w, 0, pad_h), mode='constant', value=0.)
+        if label is not None:
+            label = F.pad(label, (0, pad_w, 0, pad_h), mode='constant', 
+                          value=settings.IGNORE_LABEL)
+    return image, label
 
-    for i, [image, label] in enumerate(dt_iter):
-        sess.inf_batch(image, label)
-        if i % 10 == 0:
-            logger.info('num-%d' % i)
-            scores, cls_iu = cal_scores(sess.hist.cpu().numpy())
-            for k, v in scores.items():
-                logger.info('%s-%f' % (k, v))
 
-    scores, cls_iu = cal_scores(sess.hist.cpu().numpy())
-    for k, v in scores.items():
-        logger.info('%s-%f' % (k, v))
-    logger.info('')
-    for k, v in cls_iu.items():
-        logger.info('%s-%f' % (k, v))
+def pad_inf(image, label=None):
+    h, w = image.size()[-2:] 
+    stride = settings.STRIDE
+    pad_h = (stride + 1 - h % stride) % stride
+    pad_w = (stride + 1 - w % stride) % stride
+    if pad_h > 0 or pad_w > 0:
+        image = F.pad(image, (0, pad_w, 0, pad_h), mode='constant', value=0.)
+        if label is not None:
+            label = F.pad(label, (0, pad_w, 0, pad_h), mode='constant', 
+                          value=settings.IGNORE_LABEL)
+    return image, label
+
+
+def crop(image, label=None):
+    h, w = image.size()[-2:]
+    crop_size = settings.CROP_SIZE
+    s_h = np.random.randint(0, h - crop_size + 1)
+    s_w = np.random.randint(0, w - crop_size + 1)
+    e_h = s_h + crop_size
+    e_w = s_w + crop_size
+    image = image[:, :, s_h: e_h, s_w: e_w]
+    label = label[:, :, s_h: e_h, s_w: e_w]
+    return image, label
+
+
+def flip(image, label=None):
+    if np.random.rand() < 0.5:
+        image = torch.flip(image, [3])
+        if label is not None:
+            label = torch.flip(label, [3])
+    return image, label
+
+
+class BaseDataset(data.Dataset):
+    def __init__(self, data_root, split):
+        self.data_root = data_root
+
+        file_list = osp.join('datasets', 'nyu', split, split + '.txt')
+        file_list = tuple(open(file_list, 'r'))
+        file_list = [id_.rstrip() for id_ in file_list]
+        self.files = file_list
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        image_id = self.files[idx]
+        return self._get_item(image_id)
+
+    def _get_item(self, idx):
+        raise NotImplemented
+
+
+class TrainDataset(BaseDataset):
+    def __init__(self, data_root=settings.DATA_ROOT, split='train'):
+        super(TrainDataset, self).__init__(data_root, split)
+
+    def _get_item(self, image_id):
+        image_path = osp.join(self.data_root, 'train', 'images', image_id + '.png')
+        label_path = osp.join(self.data_root, 'train', 'GT', image_id + '.png')
+        
+        image, label = fetch(image_path, label_path)
+        image, label = scale(image, label)
+        image, label = pad(image, label)
+        image, label = crop(image, label)
+        image, label = flip(image, label)
+
+        return image[0], label[0, 0].long()
+ 
+
+class ValDataset(BaseDataset):
+    def __init__(self, data_root=settings.DATA_ROOT, split='val'):
+        super(ValDataset, self).__init__(data_root, split)
+
+    def _get_item(self, image_id):
+        image_path = osp.join(self.data_root, 'val', 'images', image_id + '.png')
+        label_path = osp.join(self.data_root, 'val', 'GT', image_id + '.png')
+        
+
+        image, label = fetch(image_path, label_path)
+        image, label = pad_inf(image, label)
+        return image[0], label[0, 0].long()
+
+
+
+def test_dt():
+    train_dt = TrainDataset()
+    print('train', len(train_dt))
+    for i in range(10):
+        img, lbl = train_dt[i]
+        print(img.shape, lbl.shape, img.mean(), np.unique(lbl))
+
+    val_dt = ValDataset()
+    print('val', len(val_dt))
+    for i in range(10):
+        img, lbl = val_dt[i]
+        print(img.shape, lbl.shape, img.mean(), np.unique(lbl))
 
 
 if __name__ == '__main__':
-    main()
+    test_dt()
